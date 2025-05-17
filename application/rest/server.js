1b9c836c89f4aa9d8e608e936eeeb8767bfba73e
DEBUGGING123 <robin9898@naver.com>
2025-05-17 11:38:01 +0900

    add card

--- application/rest/server.js
+++ application/rest/server.js
@@ -5,40 +5,24 @@ let sdk = require('./sdk');
 
 const PORT = 8001;
 const HOST = '0.0.0.0';
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/init', function (req, res) {
-    let a = req.query.a;
-    let aval = req.query.aval;
-    let b = req.query.b;
-    let bval = req.query.bval;
-    let args = [a, aval, b, bval];
-    sdk.send(false, 'Init', args, res);
- });
-
-app.get('/invoke', function (req, res) {
-    let a = req.query.a;
-    let b = req.query.b;
-    let value = req.query.value;
-    let args = [a, b, value];
-    sdk.send(false, 'Invoke', args, res);
+app.post('/init', function (req, res) {
+   const {CardName, CardNum, Username, Exdate, Password} = req.body;
+   const args = [CardName, CardNum, Username, Exdate, Password];
+   sdk.send(false, 'Init', args, res);
 });
 
-app.get('/query', function (req, res) {
-    let name = req.query.name;
-    let args = [name];
-    sdk.send(true, 'Query', args, res);
- });
-
- app.get('/delete', function (req, res) {
-    let name = req.query.name;
-    let args = [name];
-    sdk.send(false, 'Delete', args, res);
+app.get('/add_balance', function (req, res) {
+   const {CardName, amount} = req.query;
+   const args = [CardName, amount.toString()];
+   sdk.send(false, 'AddBalance', args, res);
 });
 
-app.get('/queryAll', function (req, res) {
-    sdk.send(true, 'GetAllQuery', [], res);
+app.get('/queryall', function (req, res) {
+   sdk.send(true, 'GetAllQuery', [], res);
 });
 
 app.use(express.static(path.join(__dirname, '../client')));
